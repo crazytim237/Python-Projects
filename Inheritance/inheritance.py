59745111eb45173ea7125d7662a01679c047b5e0
crazytim237 <87796455+crazytim237@users.noreply.github.com>
2021-12-20 21:09:38 -0500

    UPDATE

---
+++ Inheritance/inheritance.py
@@ -0,0 +1,25 @@
+class User:
+    name = 'No Name Provided'
+    email = ''
+    password = '1234abcd'
+    account_number = 0
+
+class Employee (User):
+    base_pay = 11.00
+    department = 'General'
+    
+class Customer (User):
+    mailing_address = ''
+    mailing_list True
+
+
+
+class Vehicle:
+    Energy siurce = Gasoline engine
+    Physucal form = Pickup truck
+    Number of doors: 2
+
+class Brand (Vehicle)
+    Company: Ford
+
+    
